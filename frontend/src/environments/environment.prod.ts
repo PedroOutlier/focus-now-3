5127f6a5bcfda5cc27d3347156e0a211ae80b220
pedrooutlier <profpedroennet@gmail.com>
2025-11-06 02:04:52 -0300

    https://github.com/PedroOutlier/FocusNow-3.git

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   showCelebrationTester: false,
-  apiBaseUrl: 'http://10.87.20.166:3000',
+  apiBaseUrl: 'http://192.168.0.6:3000',
   //'https://your-api-url.com',
   // For APK hitting an HTTP API over LAN, use bearer tokens instead of cookies
   useCookies: false
